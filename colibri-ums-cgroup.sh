558b25b981b33f6de026ba1d59a4a2db4502138e
davidjenei <info@davidjenei.com>
2020-09-09 11:32:10 +0200

    Fix ums device type (char --> block) in cgroup allow list

--- colibri-ums-cgroup.sh
+++ colibri-ums-cgroup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-echo c $1:$2 rwm | tee /sys/fs/cgroup/devices/docker/$(docker inspect --format="{{.Id}}" $4)/devices.$3
+echo b $1:$2 rwm | tee /sys/fs/cgroup/devices/docker/$(docker inspect --format="{{.Id}}" $4)/devices.$3
